cdb81601a849c0c536c4ac04d33dae46a8a436ba
VK Johnson <64655526+VKS-8@users.nonreply.github.com>
2022-10-29 23:52:09 -0500

    added new files Browser-support.txt Cognizant-IBM-Course-Project Edited file firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,5 @@
-# This is course code per instructions
+# This is course code per course instructions
 # Display the output
 print ("New Python file")
+# This is an edit to this file per course instructions
+print ("Edit to this file")
